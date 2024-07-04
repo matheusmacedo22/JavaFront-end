32a06f644440a458880ea7dc00594ca576e1bf42
Matheus Macedo <147095842+matheusmacedo22@users.noreply.github.com>
2024-07-04 12:29:55 -0300

    Add files via upload

---
+++ Operadores.java
@@ -0,0 +1,33 @@
+public class Operadores { // Início da classe Operadores
+    public static void main(String[] args) throws Exception {  // Início do método main
+        System.out.println("Operador de atribuição: =");  // Exibe "Operador de atribuição: ="
+        String texto = "Texto";  // Declaração da variável texto, do tipo String, com o valor "Texto"
+        System.out.println("String texto = \"Texto\";");  // Exibe "String texto = \"Texto\";"
+        System.out.println(texto);  // Exibe o valor da variável texto 
+
+        System.out.println("Operador de soma: +"); // Exibe "Operador de soma: +"
+        int soma = 1 + 1;  // Declaração da variável soma, do tipo int, com o valor "1 + 1"
+        System.out.println("int soma = 1 + 1;");  // Exibe "int soma = 1 + 1;"
+        System.out.println(soma);  // Exibe o valor da variável soma
+
+        System.out.println("Operador de subtração: -"); // Exibe "Operador de subtração: -"
+        int subtracao = 1 - 1;  // Declaração da variável subtracao, do tipo int, com o valor "1 - 1"
+        System.out.println("int subtracao = 1 - 1;");  // Exibe "int subtracao = 1 - 1;"
+        System.out.println(subtracao);  // Exibe o valor da variável subtracao
+
+        System.out.println("Operador de divisão: /");  // Exibe "Operador de divisão: /"
+        int divisao = 1 / 1;  // Declaração da variável divisao, do tipo int, com o valor "1 / 1"
+        System.out.println("int divisao = 1 / 1;");  // Exibe "int divisao = 1 / 1;"
+        System.out.println(divisao);  // Exibe o valor da variável divisao
+
+        System.out.println("Operador de multiplicação: *");  // Exibe "Operador de multiplicação: *"
+        int multiplicacao = 1 * 1;  // Declaração da variável multiplicacao, do tipo int, com o valor "1 * 1"
+        System.out.println("int multiplicacao = 1 * 1;");  // Exibe "int multiplicacao = 1 * 1;"
+        System.out.println(multiplicacao);  // Exibe o valor da variável multiplicacao
+
+        System.out.println("Operador de concatenação de strings: +");  // Exibe "Operador de concatenação de strings: +"
+        String concatenacao = "Texto 1 + " + "Texto 2";  // Declaração da variável concatenacao, do tipo String, com o valor "Texto 1 + Texto 2"
+        System.out.println("String concatenacao = \"Texto 1 + \" + \"Texto 2\";");  // Exibe "String concatenacao = \"Texto 1 + \" + \"Texto 2\";"
+        System.out.println(concatenacao);  // Exibe o valor da variável concatenacao
+    } // Fim do método main
+} // Fim da classe Operadores
