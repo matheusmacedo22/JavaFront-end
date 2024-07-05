1fb1de955ae55110efc48b8eaa3c265de21c751b
Matheus Macedo <147095842+matheusmacedo22@users.noreply.github.com>
2024-07-05 14:17:16 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,27 @@
+public class Variaveis{ // Declaração de uma classe pública chamada Variáveis
+public static void main(String[] args) throws Exception { // Declaração do método principal
+    int varInt = 2; // Declaração da variável varInt, do tipo int, com valor recebido 2
+    System.out.println("Variável do tipo int (inteiro): " + varInt); 
+
+    Integer varInteger = 3; // Declaração da variável varInteger, do tipo Integer, com valor recebido 3
+    System.out.println("Variável do tipo integer (inteiro): " + varInteger); 
+
+    String varString = "texto"; // Declaração da variável varString, do tipo String, com valor recebido texto
+    System.out.println("Variável do tipo String (Texto): " + varString);  
+
+    boolean varBoolean = true; // Declaração da variável varBoolean, do tipo Boolean, com valor recebido true
+    System.out.println("Variável do tipo boolean (valores lógicos): " + varBoolean);  
+
+    byte varByte = 1; // Declaração da variável varByte, do tipo byte, com valor recebido 1
+    System.out.println("Variável do tipo byte (binário): " + varByte); 
+
+    char varChar = 'r'; // Declaração da variável varChar, do tipo char, com valor recebido r
+    System.out.println("Variável do tipo char (caractere): " + varChar); 
+
+    float varFloat = 19.99f; // Declaração da variável varFloat, do tipo float, com valor recebido 19.99
+    System.out.println("Variável do tipo float (ponto flutuante): " + varFloat); 
+
+    double varDouble = 0.263568954; // Declaração da variável varDouble, do tipo double, com valor recebido  0.263568954
+    System.out.println("Variável do tipo double (ponto flutuante): " + varDouble); 
+}
+}
